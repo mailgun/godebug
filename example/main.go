7f7f1ea2bc3035ba3aa375383136cbae335d020a
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2015-01-16 14:21:59 -0800

    move debug.go to new debug package

--- example/main.go
+++ example/main.go
@@ -4,68 +4,69 @@ import (
 	"fmt"
 
 	"github.com/bradfitz/iter"
+	"github.com/jeremyschlatter/godebug"
 )
 
 func main() {
-	Line()
+	godebug.Line()
 	x := mul(1, 2)
-	RecordVars(&x, "x")
-	defer OutOfScope("x")
-	SetTrace()
-	Line()
+	godebug.RecordVars(&x, "x")
+	defer godebug.OutOfScope("x")
+	godebug.SetTrace()
+	godebug.Line()
 	x = mul(x, x)
-	Line()
+	godebug.Line()
 	if x == 4 {
-		Line()
+		godebug.Line()
 		fmt.Println("It works! x == 4.")
 	} else if n := func() int {
-		ElseIfSimpleStmt("} else if n := 2; n == 3 {")
+		godebug.ElseIfSimpleStmt("} else if n := 2; n == 3 {")
 		return 2
 	}(); func() bool {
-		ElseIfExpr("} else if n := 2; n == 3 {")
+		godebug.ElseIfExpr("} else if n := 2; n == 3 {")
 		return n == 3
 	}() {
-		Line()
+		godebug.Line()
 		fmt.Println("Math is broken. Run.")
 	} else {
-		SLine("} else {")
-		Line()
+		godebug.SLine("} else {")
+		godebug.Line()
 		fmt.Println("What's going on? x ==", x)
 	}
 }
 
 func add(n, m int) int {
-	EnterFunc()
-	defer ExitFunc()
-	Line()
+	godebug.EnterFunc()
+	defer godebug.ExitFunc()
+	godebug.Line()
 	if n == 0 {
-		Line()
+		godebug.Line()
 		return m
 	}
-	Line()
+	godebug.Line()
 	if m == 0 {
-		Line()
+		godebug.Line()
 		return n
 	}
-	Line()
+	godebug.Line()
 	return n + m
 }
 
 func mul(n, m int) int {
-	EnterFunc()
-	defer ExitFunc()
-	RecordVars(&n, "n", &m, "m")
-	defer OutOfScope("n", "m")
-	Line()
+	godebug.EnterFunc()
+	defer godebug.ExitFunc()
+	godebug.RecordVars(&n, "n", &m, "m")
+	defer godebug.OutOfScope("n", "m")
+	godebug.Line()
 	var r int
-	RecordVars(&r, "r")
-	defer OutOfScope("r")
-	Line()
+	godebug.RecordVars(&r, "r")
+	defer godebug.OutOfScope("r")
+	godebug.Line()
 	for range iter.N(m) {
-		Line()
+		godebug.Line()
 		r += m
-		SLine("for range iter.N(m) {")
+		godebug.SLine("for range iter.N(m) {")
 	}
-	Line()
+	godebug.Line()
 	return r
 }
