0fc65b89fca3e5a1fe3323f64c3e1a3b3467179e
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2015-01-16 23:53:17 -0800

    cmd: refactor RecordVars code and add OutOfScope calls for function blocks and for/range statements

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,8 @@ import (
 	"strconv"
 
 	"golang.org/x/tools/go/ast/astutil"
+	_ "golang.org/x/tools/go/gcimporter"
+	"golang.org/x/tools/go/types"
 )
 
 // visitFn is a wrapper to make plain functions implement the ast.Visitor interface.
@@ -21,6 +23,8 @@ func (v visitFn) Visit(n ast.Node) ast.Visitor {
 	return v(n)
 }
 
+var defs = make(map[*ast.Ident]types.Object)
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Must pass a single *.go file.")
@@ -30,6 +34,10 @@ func main() {
 	parsed, err := parser.ParseFile(fs, os.Args[1], nil, 0)
 	if err != nil {
 		log.Fatalf("error during parsing: %v", err)
+	}
+	_, err = (&types.Config{}).Check(parsed.Name.Name, fs, []*ast.File{parsed}, &types.Info{Defs: defs})
+	if err != nil {
+		log.Fatalf("error during type checking: %v", err)
 	}
 	astutil.AddImport(fs, parsed, "github.com/jeremyschlatter/godebug")
 	ast.Walk(visitFn(process), parsed)
@@ -63,90 +71,98 @@ func processIf(ifstmt *ast.IfStmt) {
 }
 
 func processFor(forstmt *ast.ForStmt) {
-	processBlock(forstmt.Body)
+	cleanup := processBlock(forstmt.Body)
+	if cleanup != nil {
+		forstmt.Body.List = append(forstmt.Body.List, &ast.ExprStmt{
+			X: cleanup,
+		})
+	}
 }
 
 func processRange(rangestmt *ast.RangeStmt) {
-	processBlock(rangestmt.Body)
-}
-
-// appendIdentToRecordVars adds a new identifier to a godebug.RecordVars call.
-// Given an *ast.Ident that corresponds to a variable "foo" and an *ast.CallExpr
-// that corresponds to a call `godebug.RecordVars(&x, "x")`, this function
-// modifies the call to the ast that corresponds to the new call
-// `godebug.RecordVars(&x, "x", &foo, "foo")`.
-func appendIdentToRecordVars(call *ast.CallExpr, ident *ast.Ident) {
-	// Don't record the blank identifier.
-	if ident.Name == "_" {
-		return
+	cleanup := processBlock(rangestmt.Body)
+	if cleanup != nil {
+		rangestmt.Body.List = append(rangestmt.Body.List, &ast.ExprStmt{
+			X: cleanup,
+		})
 	}
-	call.Args = append(call.Args, []ast.Expr{
-		&ast.UnaryExpr{
-			Op: token.AND,
-			X:  ident,
-		},
-		&ast.BasicLit{
-			Kind:  token.STRING,
-			Value: strconv.Quote(ident.Name),
-		},
-	}...)
 }
 
-func recordVarsIfNeeded(stmt ast.Stmt) *ast.ExprStmt {
+func listNewIdents(stmt ast.Stmt) []*ast.Ident {
 	switch i := stmt.(type) {
 	case *ast.DeclStmt:
-		return recordVarsFromDecl(i.Decl.(*ast.GenDecl))
+		return listNewIdentsFromDecl(i.Decl.(*ast.GenDecl))
 	case *ast.AssignStmt:
-		return recordVarsFromAssign(i)
+		return listNewIdentsFromAssign(i)
 	default:
 		return nil
 	}
 }
 
-// recordVarsFromDecl is for declarations using the keyword "var"
-func recordVarsFromDecl(decl *ast.GenDecl) *ast.ExprStmt {
+func isNewIdent(ident *ast.Ident) bool {
+	return ident.Name != "_" && defs[ident] != nil
+}
+
+// listNewIdentsFromDecl is for declarations using the keyword "var"
+func listNewIdentsFromDecl(decl *ast.GenDecl) (idents []*ast.Ident) {
 	if decl.Tok != token.VAR {
-		return nil
+		return
 	}
-	call := newGodebugCall("RecordVars")
 	for _, specs := range decl.Specs {
 		for _, ident := range specs.(*ast.ValueSpec).Names {
-			appendIdentToRecordVars(call, ident)
+			if isNewIdent(ident) {
+				idents = append(idents, ident)
+			}
 		}
 	}
-	if len(call.Args) == 0 {
-		return nil
-	}
-	return &ast.ExprStmt{
-		X: call,
-	}
+	return
 }
 
-// recordVarsFromAssign is for short variable declarations
-func recordVarsFromAssign(assign *ast.AssignStmt) *ast.ExprStmt {
-	// Ignore plain assignments. We're just looking for declarations.
-	if assign.Tok != token.DEFINE {
-		return nil
-	}
-	call := newGodebugCall("RecordVars")
+// listNewIdentsFromAssign is for short variable declarations
+func listNewIdentsFromAssign(assign *ast.AssignStmt) (idents []*ast.Ident) {
 	for _, expr := range assign.Lhs {
-		if ident, ok := expr.(*ast.Ident); ok {
-			appendIdentToRecordVars(call, ident)
+		if ident, ok := expr.(*ast.Ident); ok && isNewIdent(ident) {
+			idents = append(idents, ident)
 		}
 	}
-	if len(call.Args) == 0 {
-		return nil
-	}
-	return &ast.ExprStmt{
-		X: call,
-	}
+	return
 }
 
-func processBlock(blk *ast.BlockStmt) {
+func recordVars(idents []*ast.Ident) ast.Stmt {
+	expr := newGodebugExpr("RecordVars")
+	call := expr.X.(*ast.CallExpr)
+	call.Args = make([]ast.Expr, 2*len(idents))
+	for i, ident := range idents {
+		call.Args[2*i] = &ast.UnaryExpr{
+			Op: token.AND,
+			X:  ident,
+		}
+		call.Args[2*i+1] = &ast.BasicLit{
+			Kind:  token.STRING,
+			Value: strconv.Quote(ident.Name),
+		}
+	}
+	return expr
+}
+
+func outOfScopeVars(idents []*ast.Ident) *ast.CallExpr {
+	call := newGodebugCall("OutOfScope")
+	call.Args = make([]ast.Expr, len(idents))
+	for i, ident := range idents {
+		call.Args[i] = &ast.BasicLit{
+			Kind:  token.STRING,
+			Value: strconv.Quote(ident.Name),
+		}
+	}
+	return call
+}
+
+func processBlock(blk *ast.BlockStmt) (cleanupCall *ast.CallExpr) {
 	if blk == nil {
 		return
 	}
 	newBody := make([]ast.Stmt, 0, 2*len(blk.List))
+	var scopedIdents []*ast.Ident
 	for _, stmt := range blk.List {
 		newBody = append(newBody, newGodebugExpr("Line"))
 		if ifstmt, ok := stmt.(*ast.IfStmt); ok {
@@ -159,11 +175,17 @@ func processBlock(blk *ast.BlockStmt) {
 			processRange(forstmt)
 		}
 		newBody = append(newBody, stmt)
-		if call := recordVarsIfNeeded(stmt); call != nil {
-			newBody = append(newBody, call)
+		newIdents := listNewIdents(stmt)
+		if len(newIdents) > 0 {
+			newBody = append(newBody, recordVars(newIdents))
+			scopedIdents = append(scopedIdents, newIdents...)
 		}
 	}
 	blk.List = newBody
+	if len(scopedIdents) > 0 {
+		cleanupCall = outOfScopeVars(scopedIdents)
+	}
+	return cleanupCall
 }
 
 func process(node ast.Node) ast.Visitor {
@@ -174,14 +196,20 @@ func process(node ast.Node) ast.Visitor {
 	if !ok {
 		return nil
 	}
-	processBlock(fn.Body)
+	cleanupCall := processBlock(fn.Body)
+	prepend := []ast.Stmt{
+		newGodebugExpr("EnterFunc"),
+		&ast.DeferStmt{
+			Call: newGodebugCall("ExitFunc"),
+		},
+	}
+	if cleanupCall != nil {
+		prepend = append(prepend, &ast.DeferStmt{
+			Call: cleanupCall,
+		})
+	}
 	if fn.Body != nil {
-		fn.Body.List = append([]ast.Stmt{
-			newGodebugExpr("EnterFunc"),
-			&ast.DeferStmt{
-				Call: newGodebugCall("ExitFunc"),
-			},
-		}, fn.Body.List...)
+		fn.Body.List = append(prepend, fn.Body.List...)
 	}
 	return nil
 }
