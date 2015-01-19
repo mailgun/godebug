2a9479f2b1edb25148ab063eb5cbe4908b9dd4f3
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2015-01-19 02:55:03 -0800

    example: I accidentally checked in the generated version. Undo that, and also remove bradfitz/iter import.

--- example/main.go
+++ example/main.go
@@ -3,76 +3,36 @@ package main
 import (
 	"fmt"
 
-	"github.com/bradfitz/iter"
 	"github.com/mailgun/godebug/lib"
 )
 
-var main_goScope = godebug.EnteringNewScope()
-
 func main() {
-	godebug.Line()
 	x := mul(1, 2)
-	godebugScope := main_goScope.EnteringNewChildScope()
-	defer godebugScope.End()
-	godebugScope.Declare("x", &x)
 	godebug.SetTrace()
-	godebug.Line()
 	x = mul(x, x)
-	godebug.Line()
 	if x == 4 {
-		godebug.Line()
 		fmt.Println("It works! x == 4.")
-	} else if n := func() int {
-		godebug.ElseIfSimpleStmt("} else if n := 2; n == 3 {")
-		return 2
-	}(); func() bool {
-		godebug.ElseIfExpr("} else if n := 2; n == 3 {")
-		return n == 3
-	}() {
-		godebug.Line()
+	} else if n := 2; n == 3 {
 		fmt.Println("Math is broken. Run.")
 	} else {
-		godebug.SLine("} else {")
-		godebug.Line()
 		fmt.Println("What's going on? x ==", x)
 	}
 }
 
 func add(n, m int) int {
-	godebug.EnterFunc()
-	defer godebug.ExitFunc()
-	godebugScope := main_goScope.EnteringNewChildScope()
-	defer godebugScope.End()
-	godebugScope.Declare("n", &n, "m", &m)
-	godebug.Line()
 	if n == 0 {
-		godebug.Line()
 		return m
 	}
-	godebug.Line()
 	if m == 0 {
-		godebug.Line()
 		return n
 	}
-	godebug.Line()
 	return n + m
 }
 
 func mul(n, m int) int {
-	godebug.EnterFunc()
-	defer godebug.ExitFunc()
-	godebugScope := main_goScope.EnteringNewChildScope()
-	defer godebugScope.End()
-	godebugScope.Declare("n", &n, "m", &m)
-	godebug.Line()
 	var x int
-	godebugScope.Declare("x", &x)
-	godebug.Line()
-	for range iter.N(m) {
-		godebug.Line()
+	for i := 0; i < m; i++ {
 		x = add(x, m)
-		godebug.SLine("for range iter.N(m) {")
 	}
-	godebug.Line()
 	return x
 }
