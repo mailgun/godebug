16fe7f78c27bde9c1f883e132d831613748acf7e
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2015-01-27 10:28:50 -0800

    update func-lit-out.go

--- golden_tests/func-lit-out.go
+++ golden_tests/func-lit-out.go
@@ -2,32 +2,43 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/mailgun/godebug/lib"
 )
 
 var func_lit_in_goScope = godebug.EnteringNewScope()
 
 func main() {
-	godebug.Line()
+	ctx, ok := godebug.EnterFunc(func() {
+		main()
+	})
+	if !ok {
+		return
+	}
+	godebug.Line(ctx, func_lit_in_goScope)
 	hi, there := foo(7, 12)
 	godebugScope := func_lit_in_goScope.EnteringNewChildScope()
-	defer godebugScope.End()
 	godebugScope.Declare("hi", &hi, "there", &there)
-	godebug.Line()
+	godebug.Line(ctx, godebugScope)
 	fmt.Println(hi, there)
 }
 
 var foo = func(a, _ int) (b, _ string) {
 	var godebugResult2 string
-	godebug.EnterFunc(func() {
+	var ctx *godebug.Context
+	fn := func() {
 		b, godebugResult2 = func() (b, _ string) {
 			godebugScope := func_lit_in_goScope.EnteringNewChildScope()
-			defer godebugScope.End()
 			godebugScope.Declare("a", &a, "b", &b)
-			godebug.Line()
+			godebug.Line(ctx, godebugScope)
 			return "Hello", "World"
 		}()
-	})
+	}
+	var ok bool
+	ctx, ok = godebug.EnterFunc(fn)
+	if ok {
+		fn()
+	}
 	godebug.ExitFunc()
 	return b, godebugResult2
 }
