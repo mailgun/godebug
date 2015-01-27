b20ccfad33da23284c7c204cd1b553792fb8bdc0
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2015-01-27 10:32:02 -0800

    update unnamed_input-out.go

--- golden_tests/unnamed_input-out.go
+++ golden_tests/unnamed_input-out.go
@@ -5,19 +5,26 @@ import "github.com/mailgun/godebug/lib"
 var unnamed_input_in_goScope = godebug.EnteringNewScope()
 
 func main() {
-	godebug.Line()
+	ctx, ok := godebug.EnterFunc(func() {
+		main()
+	})
+	if !ok {
+		return
+	}
+	godebug.Line(ctx, unnamed_input_in_goScope)
 	foo(3)
 }
 
 func foo(int) string {
 	var godebugInput1 int
 	var godebugResult1 string
-	if !godebug.EnterFunc(func() {
+	ctx, ok := godebug.EnterFunc(func() {
 		godebugResult1 = foo(godebugInput1)
-	}) {
+	})
+	if !ok {
 		return godebugResult1
 	}
 	defer godebug.ExitFunc()
-	godebug.Line()
+	godebug.Line(ctx, unnamed_input_in_goScope)
 	return "hello"
 }
