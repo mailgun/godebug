09e96158ea1cb2bc4c5e277476361d97c27c5cfd
hjusforgues <hector.jusforgues@lazada.com>
2015-06-19 17:28:20 +0700

    Add a test showing 'multiple var declaration' bug

---
+++ testdata/single-file-tests/multiple-var-declaration-in.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	func() {
+		var (
+			x int
+			y int
+			err error
+		)
+		x = 2
+		y = 3
+		err = nil
+		if err != nil {
+			fmt.Printf("%d\n", x+y)
+		}
+	}()
+}
